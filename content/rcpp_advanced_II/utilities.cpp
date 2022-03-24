f14998ddf26e8b93ddd4387cda2486e1d4b742b2
removedporn <86824510+removedporn@users.noreply.github.com>
2022-03-24 09:15:49 +0800

    Delete content directory

--- content/rcpp_advanced_II/utilities.cpp
+++
@@ -1,6 +0,0 @@
-
-#include "utilities.h"
-
-double mySquare(double x){
-  return x * x;
-}
--- content/rcpp_advanced_II/utilities.h
+++
@@ -1,7 +0,0 @@
-
-#ifndef __UTILITIES__
-#define __UTILITIES__
-
-double mySquare(double x);
-
-#endif // __UTILITIES__
